edbb0661fbcb32624f31d706c11585bba60201bb
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-06 16:22:54 -0300

    feat: Implement useFetchOptions hook and related types for improved data fetching in insurance quote form

--- src/components/forms/suhai/insuranceQuote/modules/fetchOptions/index.ts
+++ src/components/forms/suhai/insuranceQuote/modules/fetchOptions/index.ts
@@ -1,42 +1,43 @@
 import type { ResponseFormOptions } from "@modules/formOptions/options/dtos"
 import { formOptions } from "@modules/formOptions/options/services"
+import type { FetchOptions } from "./types"
 
-export const fetchSexo = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'sexo' })
+export const fetchSexo = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'sexo' })
     return { ...response }
 }
 
-export const fetchEstadoCivil = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'estadoCivil' })
+export const fetchEstadoCivil = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'estadoCivil' })
     return { ...response }
 }
 
-export const fetchZeroKm = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'zeroKm' })
+export const fetchZeroKm = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'zeroKm' })
     return { ...response }
 }
 
-export const fetchTipoUtilizacao = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'tipoUtilizacao' })
+export const fetchTipoUtilizacao = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'tipoUtilizacao' })
     return { ...response }
 }
 
-export const fetchPossuiOutroVeic = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'possuiOutroVeic' })
+export const fetchPossuiOutroVeic = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'possuiOutroVeic' })
     return { ...response }
 }
 
-export const fetchTipoContratacao = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'tipoContratacao' })
+export const fetchTipoContratacao = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'tipoContratacao' })
     return { ...response }
 }
 
-export const fetchSexoPrincipalCondutor = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'sexoPrincipalCondutor' })
+export const fetchSexoPrincipalCondutor = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'sexoPrincipalCondutor' })
     return { ...response }
 }
 
-export const fetchEstadoCivilPrincipalCondutor = async (): Promise<ResponseFormOptions> => {
-    const response: ResponseFormOptions = await formOptions.options({ key: 'estadoCivilPrincipalCondutor' })
+export const fetchEstadoCivilPrincipalCondutor = async ({ signal }: FetchOptions): Promise<ResponseFormOptions> => {
+    const response: ResponseFormOptions = await formOptions.options({ signal, key: 'estadoCivilPrincipalCondutor' })
     return { ...response }
 }
