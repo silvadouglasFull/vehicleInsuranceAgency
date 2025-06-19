ddc9b40b065884b4954929c678fb07da1181926e
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-19 16:43:54 -0300

    feat: add useInsuranceQuote hook and update import paths in select input hooks

--- src/components/forms/suhai/insuranceQuote/garageData/select/pergunta1/hooks/useSelectInput/index.ts
+++ src/components/forms/suhai/insuranceQuote/garageData/select/pergunta1/hooks/useSelectInput/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useInsuranceQuote } from "@components/forms/suhai/insuranceQuote/garageData/hooks/insuranceQuote";
+import { useInsuranceQuote } from "@components/forms/suhai/insuranceQuote/garageData/context/hooks/insuranceQuote";
 import type { UseSelectInput } from "@components/forms/suhai/insuranceQuote/garageData/select/pergunta1/hooks/useSelectInput/types";
 import type { SelectedOption } from "@components/forms/suhai/insuranceQuote/renderFormSection/input/select/types";
 import { useEffect, useState } from "react";
--- src/components/forms/suhai/insuranceQuote/garageData/select/pergunta4/hooks/useSelectInput/index.ts
+++ src/components/forms/suhai/insuranceQuote/garageData/select/pergunta4/hooks/useSelectInput/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useInsuranceQuote } from "@components/forms/suhai/insuranceQuote/garageData/hooks/insuranceQuote";
+import { useInsuranceQuote } from "@components/forms/suhai/insuranceQuote/garageData/context/hooks/insuranceQuote";
 import type { UseSelectInput } from "@components/forms/suhai/insuranceQuote/garageData/select/pergunta4/types";
 import type { SelectedOption } from "@components/forms/suhai/insuranceQuote/renderFormSection/input/select/types";
 import { useEffect, useState } from "react";
