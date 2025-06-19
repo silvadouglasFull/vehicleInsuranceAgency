f51d8f95f5fc23f20d810d579c552318bd197be5
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-19 17:03:56 -0300

    refactor: consolidate select exports for estadoCivil and sexo into a single index file

---
+++ src/components/forms/suhai/insuranceQuote/personalData/select/index.ts
@@ -0,0 +1,3 @@
+export * from "@components/forms/suhai/insuranceQuote/personalData/select/estadoCivil"
+export * from "@components/forms/suhai/insuranceQuote/personalData/select/sexo"
+
--- src/components/forms/suhai/insuranceQuote/renderFormSection/input/select/personalData/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "@components/forms/suhai/insuranceQuote/personalData/select/sexo"
-export * from "@components/forms/suhai/insuranceQuote/renderFormSection/input/select/personalData/estadoCivil"
-
