14b35dbc8698d536a35a28dd5303b75ceeb05842
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-19 16:17:48 -0300

    fix: change states type from Partial<States> to States in garage data context

--- src/components/forms/suhai/insuranceQuote/garageData/context/constants/index.ts
+++ src/components/forms/suhai/insuranceQuote/garageData/context/constants/index.ts
@@ -1,5 +1,5 @@
 import type { States } from "@components/forms/suhai/insuranceQuote/garageData/context/types";
-export const states: Partial<States> = {
+export const states: States = {
     pergunta1: '',
     pergunta2: '',
     pergunta3: '',
