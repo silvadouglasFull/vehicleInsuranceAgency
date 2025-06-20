aabe8b6e837bb55092ae5a026c43c39f22dc691e
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-20 15:43:12 -0300

    feat: update API host URL to use secure HTTPS protocol

--- src/flavor/api/index.ts
+++ src/flavor/api/index.ts
@@ -1,6 +1,6 @@
 import type { ApiVersion, EndPoints, UrlString } from "@flavor/api/types"
 export const versionApi: ApiVersion = 'v0'
-export const host: UrlString = `http://localhost:3333/api/suhai` as UrlString
+export const host: UrlString = `https://onzeponto11.com.br:3333/api/suhai` as UrlString
 export const endPoints: EndPoints = {
     consultafipe: '/consultafipe',
     incluircotacao: '/incluircotacao',
