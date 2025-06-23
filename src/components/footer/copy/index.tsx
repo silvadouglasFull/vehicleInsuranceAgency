e5d63170e72f9a25f43d7054db558bba1ec065b3
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-23 13:20:54 -0300

    feat: update app version to 0.0.2 and display in footer component

--- src/components/footer/copy/index.tsx
+++ src/components/footer/copy/index.tsx
@@ -1,9 +1,10 @@
-import { nameFlavor } from "@flavor/index";
+import { appVersion, nameFlavor } from "@flavor/index";
 import type React from "react";
 export const CopyRight: React.FC = () => {
     return (
         <div className="text-center mt-3">
-            <small>&copy; {new Date().getFullYear()} {nameFlavor}. Todos os direitos reservados.</small>
+            <small className="me-2">&copy; {new Date().getFullYear()} {nameFlavor}. Todos os direitos reservados.</small>
+            <span className="text-info">Versão: {appVersion}</span>
         </div>
 
     )
--- src/flavor/index.ts
+++ src/flavor/index.ts
@@ -1,1 +1,3 @@
+import { version } from "../../package.json"
 export const nameFlavor = 'Seu Seguro no Click'
+export const appVersion = version
