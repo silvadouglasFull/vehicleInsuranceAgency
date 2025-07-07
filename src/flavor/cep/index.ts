38b44e59b895a060e45d39488fae12c0a3aa71a6
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-07 16:26:48 -0300

    fix: update API host URLs to use production endpoint

--- src/flavor/cep/index.ts
+++ src/flavor/cep/index.ts
@@ -1,5 +1,5 @@
 import type { EndPoints, UrlString } from "@flavor/cep/types"
-export const host: UrlString = 'http://localhost:3333/api' as UrlString
+export const host: UrlString = 'https://onzeponto11.com.br:3333/api' as UrlString
 export const endPoints: EndPoints = {
     consultCEP: (cep: string) => `/cep/${cep}`
 } 
--- src/flavor/whatsapp/index.ts
+++ src/flavor/whatsapp/index.ts
@@ -1,6 +1,6 @@
 import type { EndPoints, UrlString } from "@flavor/whatsapp/types"
 import type { KeyOfConfig } from "@modules/whatsapp/quotationCompletion/dto"
-export const host: UrlString = 'http://localhost:3333/api/whatsapp' as UrlString
+export const host: UrlString = 'https://onzeponto11.com.br:3333/api/whatsapp' as UrlString
 export const endPoints: EndPoints = {
     send: '/send'
 }
